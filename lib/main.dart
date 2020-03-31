f35f4e77757b09fa1c0116f494295bbe28da64d5
unknown <samip425@gmail.com>
2020-03-31 13:23:28 +0545

    added drawer

--- lib/main.dart
+++ lib/main.dart
@@ -21,57 +21,62 @@ class Work {
   Work({this.title, this.subTitle});
 }
 
-List<Work> todoList = [
-  
-];
+List<Work> todoList = [];
 
 ///////////////////
 ///card Template
-class Task extends StatelessWidget{
-    final String _title;
+class Task extends StatelessWidget {
+  final String _title;
   final String _subtitle;
   final VoidCallback onDelete;
   final VoidCallback onComplete;
 
-  Task(this._title ,this._subtitle,{this.onDelete,this.onComplete});
-  
+  Task(this._title, this._subtitle, {this.onDelete, this.onComplete});
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       child: Card(
-        color: Colors.yellow[50],
-      child:Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-           ListTile(
-            leading:Icon(Icons.work),
-            title:Text(_title),
-            subtitle: Text(_subtitle),
-          ),
-          ButtonBar(
+          color: Colors.yellow[50],
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              FlatButton(
-                child:Text("Completed",style:TextStyle(color: Colors.orangeAccent,fontFamily: "Roboto",fontSize:15)),
-                onPressed: (){
-                  onComplete();
-                },
+              ListTile(
+                leading: Icon(Icons.work),
+                title: Text(_title),
+                subtitle: Text(_subtitle),
               ),
-              FlatButton(
-                onPressed:(){
-                  onDelete();
-                } ,
-                child: Text("Remove",style:TextStyle(color: Colors.red,fontFamily: "Roboto",fontSize:15)),
-                )
+              ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: Text("Completed",
+                        style: TextStyle(
+                            color: Colors.orangeAccent,
+                            fontFamily: "Roboto",
+                            fontSize: 15)),
+                    onPressed: () {
+                      onComplete();
+                    },
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      onDelete();
+                    },
+                    child: Text("Remove",
+                        style: TextStyle(
+                            color: Colors.red,
+                            fontFamily: "Roboto",
+                            fontSize: 15)),
+                  )
+                ],
+              )
             ],
-          )
-      ],)
-    ) ,
+          )),
     );
   }
 }
 
 ///////////
-
 
 class TodoList extends StatefulWidget {
   @override
@@ -79,53 +84,76 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<TodoList> {
+  int taskDone = 0;
+  void removeItem(String title) {
+    setState(() {
+      todoList.removeWhere((item) => item.title == title);
+    });
+  }
 
-  int count=0;
-   void removeItem(String title){
-     setState(() {
-        todoList.removeWhere((item)=>item.title==title);
-     });
-   }
-   void taskCompleted(String title){
-     setState(() {
-       count++;
-        todoList.removeWhere((item)=>item.title==title);
-     });
-   }
+  void taskCompleted(String title) {
+    setState(() {
+      taskDone++;
+      todoList.removeWhere((item) => item.title == title);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow[100],
+      drawer: Drawer(
+        child: Container(
+          color: Colors.yellow[100],
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(child: Icon(Icons.account_circle, size: 70)),
+              ListTile(
+                title: Text(
+                  "Completed Task :         $taskDone",
+                  style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Colors.orangeAccent,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.calendar_today),
-        ),
         title: Text("TodoList"),
       ),
-      body: ListView(
-        children: <Widget>[
-          Container(
-              margin: const EdgeInsets.fromLTRB(0, 15, 0, 15),
-              child: Center(
-                child: Text("Today's Task",
-                    style: TextStyle(
-                        fontFamily: "Roboto",
-                        fontSize: 30,
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.orange)),
-              )),
-          Column(
-            children: todoList.map((currentTask) {
-              return Task(currentTask.title, currentTask.subTitle,onDelete:()=> removeItem(currentTask.title),onComplete:()=>taskCompleted(currentTask.title));
-            }).toList(),
-          ),
-          Text('Completed task : $count')
-        ],
-      ),
+      body: todoList.length == 0
+          ? Center(
+              child: Text('Add task to TodoList',
+                  style: TextStyle(
+                      fontFamily: "Roboto",
+                      fontSize: 30,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.orange)))
+          : (ListView(
+              children: <Widget>[
+                Container(
+                    margin: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                    child: Center(
+                      child: Text("Today's Task",
+                          style: TextStyle(
+                              fontFamily: "Roboto",
+                              fontSize: 30,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.orange)),
+                    )),
+                Column(
+                  children: todoList.map((currentTask) {
+                    return Task(currentTask.title, currentTask.subTitle,
+                        onDelete: () => removeItem(currentTask.title),
+                        onComplete: () => taskCompleted(currentTask.title));
+                  }).toList(),
+                ),
+              ],
+            )),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
@@ -194,36 +222,35 @@ class _FillupState extends State<Fillup> {
           ),
           title: Text("TodoList"),
         ),
-        body:Container(
-          margin: const EdgeInsets.only(top:20),
-          child:Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              Field(titleHandler, "Your task", "Title*"),
-              Field(subTitleHandler, "Briefly describe your task", "Sub-title"),
-              Container(
-                margin: const EdgeInsets.only(top: 50),
-                child: MaterialButton(
-                    color: Colors.orange,
-                    onPressed: () {
-                      if (_formKey.currentState.validate()) {
-                        widget.arguments.add(Work(
-                            title: titleHandler.text,
-                            subTitle: subTitleHandler.text));
-                        Navigator.pop(context);
-                      }
-                    },
-                    child: Text("Add to Todo-List"),
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12))),
-              )
-            ],
+        body: Container(
+          margin: const EdgeInsets.only(top: 20),
+          padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Field(titleHandler, "Your task", "Title*"),
+                Field(
+                    subTitleHandler, "Briefly describe your task", "Sub-title"),
+                Container(
+                  margin: const EdgeInsets.only(top: 50),
+                  child: MaterialButton(
+                      color: Colors.orange,
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          widget.arguments.add(Work(
+                              title: titleHandler.text,
+                              subTitle: subTitleHandler.text));
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Text("Add to Todo-List"),
+                      shape: new RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                )
+              ],
+            ),
           ),
-        ) ,
-        ) 
-        
-        
-        );
+        ));
   }
 }
